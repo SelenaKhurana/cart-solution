3b90e2366d1dba0b0345e2dbb2d48a65bcb122f0
Selena Khurana <76164891+SelenaKhurana@users.noreply.github.com>
2021-06-14 16:29:38 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "logo.svg";
+import './App.css';
+import Products from "products.jsx";
+
+function App() {
+  return <Products></Products>;
+}
+
+export default App;
